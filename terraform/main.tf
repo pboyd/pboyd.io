6d2a6cc2aaad1866d908f7d72b795f5b9d38a2b8
Paul Boyd <paul@pboyd.io>
2026-01-10 17:41:36 -0500

    Remove the www domain

--- terraform/main.tf
+++ terraform/main.tf
@@ -75,51 +75,6 @@ resource "aws_s3_bucket_public_access_block" "site" {
   restrict_public_buckets = true
 }
 
-# S3 bucket for www redirect
-resource "aws_s3_bucket" "www_redirect" {
-  bucket = "www.${var.domain_name}"
-
-  tags = {
-    Name        = "www.${var.domain_name}"
-    Environment = "production"
-  }
-}
-
-resource "aws_s3_bucket_website_configuration" "www_redirect" {
-  bucket = aws_s3_bucket.www_redirect.id
-
-  redirect_all_requests_to {
-    host_name = var.domain_name
-    protocol  = "https"
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "www_redirect" {
-  bucket = aws_s3_bucket.www_redirect.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
-resource "aws_s3_bucket_policy" "www_redirect" {
-  bucket = aws_s3_bucket.www_redirect.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "PublicReadGetObject"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource  = "${aws_s3_bucket.www_redirect.arn}/*"
-      }
-    ]
-  })
-}
-
 # CloudFront Origin Access Control for main site
 resource "aws_cloudfront_origin_access_control" "main" {
   name                              = var.domain_name
@@ -243,64 +198,6 @@ resource "aws_s3_bucket_policy" "site" {
   })
 }
 
-# CloudFront distribution for www redirect
-resource "aws_cloudfront_distribution" "www_redirect" {
-  enabled         = true
-  is_ipv6_enabled = true
-  aliases         = ["www.${var.domain_name}"]
-  price_class     = "PriceClass_100"
-
-  origin {
-    domain_name = aws_s3_bucket_website_configuration.www_redirect.website_endpoint
-    origin_id   = "S3-www-${var.domain_name}"
-
-    custom_origin_config {
-      http_port              = 80
-      https_port             = 443
-      origin_protocol_policy = "http-only"
-      origin_ssl_protocols   = ["TLSv1.2"]
-    }
-  }
-
-  default_cache_behavior {
-    allowed_methods        = ["GET", "HEAD"]
-    cached_methods         = ["GET", "HEAD"]
-    target_origin_id       = "S3-www-${var.domain_name}"
-    viewer_protocol_policy = "allow-all"
-    compress               = true
-
-    forwarded_values {
-      query_string = false
-      cookies {
-        forward = "none"
-      }
-    }
-
-    min_ttl     = 0
-    default_ttl = 86400
-    max_ttl     = 31536000
-  }
-
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
-
-  viewer_certificate {
-    acm_certificate_arn      = aws_acm_certificate.main.arn
-    ssl_support_method       = "sni-only"
-    minimum_protocol_version = "TLSv1.2_2021"
-  }
-
-  tags = {
-    Name        = "www.${var.domain_name}"
-    Environment = "production"
-  }
-
-  depends_on = [aws_acm_certificate_validation.main]
-}
-
 # Route 53 A record for apex domain
 resource "aws_route53_record" "main" {
   zone_id = aws_route53_zone.main.zone_id
@@ -310,19 +207,6 @@ resource "aws_route53_record" "main" {
   alias {
     name                   = aws_cloudfront_distribution.main.domain_name
     zone_id                = aws_cloudfront_distribution.main.hosted_zone_id
-    evaluate_target_health = false
-  }
-}
-
-# Route 53 A record for www subdomain
-resource "aws_route53_record" "www" {
-  zone_id = aws_route53_zone.main.zone_id
-  name    = "www.${var.domain_name}"
-  type    = "A"
-
-  alias {
-    name                   = aws_cloudfront_distribution.www_redirect.domain_name
-    zone_id                = aws_cloudfront_distribution.www_redirect.hosted_zone_id
     evaluate_target_health = false
   }
 }
